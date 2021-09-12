0bbed3a02758164d99cfae7fec8247529e66a86e
zyzera <herbachukzachary@gmail.com>
2021-09-12 11:36:44 -0500

    removed bottom sheet to set up for new one

---
+++ lib/widgets/bottom_sheet_budget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetBudget extends StatelessWidget {
+  const BottomSheetBudget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -8,93 +8,18 @@ class AddButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 0, right: 0, bottom: 15),
+      padding: const EdgeInsets.only(top: 0, left: 0, bottom: 15),
       child: Container(
         alignment: Alignment.bottomRight,
         child: FloatingActionButton.extended(
+            shape: CircleBorder(),
             backgroundColor: Colors.transparent,
             elevation: 0,
-            onPressed: () {
-              showBottomSheet(
-                context: context,
-                builder: (context) => Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    ),
-                    color: Colors.red,
-                  ),
-                  height: 500,
-                  alignment: Alignment.center,
-                  child: Expanded(
-                    child: Padding(
-                      padding:
-                          EdgeInsets.only(top: 4, right: 4, left: 4, bottom: 0),
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                            children: [
-                              Text('Add Budget Item'),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              ToggleSwitch(
-                                minWidth: 90.0,
-                                minHeight: 40.0,
-                                fontSize: 16.0,
-                                initialLabelIndex: 1,
-                                activeBgColor: [Colors.grey.shade600],
-                                activeFgColor: Colors.black,
-                                inactiveBgColor: Colors.grey[200],
-                                inactiveFgColor: Colors.grey[300],
-                                totalSwitches: 2,
-                                labels: [
-                                  'Income',
-                                  'Expense',
-                                ],
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Center(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(Icons.ac_unit),
-                                    SizedBox(
-                                      width: 20,
-                                    ),
-                                    TextField(
-                                      decoration: const InputDecoration(
-                                          border: OutlineInputBorder(),
-                                          hintText: 'Enter a name'),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30),
-                            topRight: Radius.circular(30),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            },
+            onPressed: () {},
             label: Icon(
               Icons.add_circle,
               size: 50,
-              color: Color(0xff0fd142),
+              color: Colors.green,
             )),
       ),
     );
@@ -112,6 +37,7 @@ class RemoveButton extends StatelessWidget {
       child: Container(
         alignment: Alignment.bottomLeft,
         child: FloatingActionButton.extended(
+            shape: CircleBorder(),
             backgroundColor: Colors.transparent,
             elevation: 0,
             onPressed: () {},
